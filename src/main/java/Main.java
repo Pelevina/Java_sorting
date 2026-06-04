7be649ac76d9fd1f4d04b49848ee80c045c788fd
Elena Simkina <digkiz@gmail.com>
2026-06-04 12:56:45 +1000

    Реализовано рандомизированное заполнение коллекции. 1. Добавлен класс RandomReader который генерирует список случайных игроков 2. Имена берутся из заготовленного массива, totalGames и wonGames генерируются случайно с учётом валидации 3. Добавлен вывод списка игроков на экран в Main.java (потом можно удалить, нужно для проверки что моя чатсь работает) 4. Исправлена ошибка с буфером в Main.java (добавлен in.nextLine() после nextInt())

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,10 +9,11 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter length of the collection: ");
         int lengthCollection = in.nextInt();
+        in.nextLine(); // добавила очистку буфера после nextInt()
         if (lengthCollection > 0) {
             Map<String, Reader> readerMap = new HashMap<>();
             Reader consoleReader = new ConsoleReader();
-            Reader randomReader = new RandomReader();
+            Reader randomReader = new RandomReader(lengthCollection);//добавила lengthCollection,чтобы RandomReader знал сколько игроков генерировать.
             Reader fileReader = new FileReader();
             readerMap.put(consoleReader.getReaderName(), consoleReader);
             readerMap.put(randomReader.getReaderName(), randomReader);
@@ -21,6 +22,10 @@ public class Main {
             String readerName = in.nextLine();
             if (readerMap.containsKey(readerName)) {
                 List<TableTennisPlayer> players = readerMap.get(readerName).readInput();
+                //добавила вывод на экран
+                for (TableTennisPlayer player : players) {
+                    System.out.println(player.getName() + " | games: " + player.getTotalGames() + " | won: " + player.getWonGames());
+                }
             } else {
                 System.out.println("Wrong way of input");
             }
