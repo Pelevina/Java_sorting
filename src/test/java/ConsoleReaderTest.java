658ccd4ea32a4b8d439a28d7bab8ed9982f7caaa
Denis-Vasilyev <d.v.vasilyev87@yandex.ru>
2026-06-09 16:09:51 +0300

    Скорректировал тест для ConsoleReader.

--- src/test/java/ConsoleReaderTest.java
+++ src/test/java/ConsoleReaderTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ConsoleReaderTest {
 
-    private final ByteArrayInputStream originalIn = System.in;
+    private final ByteArrayInputStream originalIn = (ByteArrayInputStream) System.in;
 
     @BeforeEach
     void setUp() {
